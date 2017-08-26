0be53229d3dff5aba64051ab49cd4370acadfa27
hishion <956656628@qq.com>
2017-08-26 16:48:50 +0800

    首页-类型、轮播

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import FastClick from 'fastclick'
+
 import VueRouter from 'vue-router'
 import App from './App'
 
@@ -17,7 +18,10 @@ Vue.use(YDUI)
 
 FastClick.attach(document.body)
 Vue.prototype.axios = axios;
+window.axios = axios;
 axios.defaults.headers={"app-version":"v1.0"}
+axios.defaults.headers['app-version']='v1.0'
+axios.defaults.baseURL='http://fenghuang.77jie.cn/gjfeng-web-client/app'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,7 +2,39 @@
     <div>
         <header-top></header-top>
         <main class="scroll-content">
-           
+            <section class="city-search flex text-center align-center danger-bg">
+                <router-link to="/city" class="current-city">
+                    <span>广州市</span>
+                    <span class="iconfont self-down"></span>
+                </router-link>
+                <router-link to="/search" class="search-box">
+                    <span class="iconfont-large self-search"></span>
+                    <span>搜索商家或商品</span>
+                </router-link>
+            </section>
+            <section class="type-list">
+                <yd-slider>
+                <yd-slider-item v-for="(item,index) in slideTypes" :key="index">
+                   <ul class="flex">
+                    <li v-for="e in item" :key="e.id" class="type-item text-center">
+                        <router-link :to="'/subColumn/'+e.id">
+                            <img :src="e.pic2" :alt="e.names">
+                            <p>{{e.names}}</p>
+                        </router-link>
+                    </li>
+                    </ul> 
+                </yd-slider-item>
+            </yd-slider>
+            </section>
+            <section class="banner-list">
+            <yd-slider>
+                <yd-slider-item v-for="(item,index) in banner" :key="item.id">
+                    <router-link :to="'/product'+item.id">
+                    <img :src="item.photo" :alt="item.names">
+                    </router-link>
+                </yd-slider-item>
+                </yd-slider>                
+            </section>
         </main>
         <footer-bar></footer-bar>
     </div>
@@ -10,17 +42,73 @@
 <script>
 import HeaderTop from 'components/header/index'
 import FooterBar from 'components/footer/index'
+
 export default {
     name: 'Home',
     data() {
         return {
-            
+            slideTypes:[],
+            banner:[],
+            productList:[],
+            pageNo:1
         }
     },
-    components: { HeaderTop, FooterBar }
-
+    components: { HeaderTop, FooterBar },
+    created() {
+        this.getColumns();
+       
+    },
+    methods: {
+        
+        getColumns() {
+            axios.post('/index/v1_0/o2o', { token: md5('gjfengo2o') })
+                .then((res) =>{
+                    // plus.nativeUI.toast(res.data.msg)
+                    let result = res.data.result;
+                    this.slideTypes = [result.indexColumn.slice(0, 8), result.indexColumn.slice(8)];
+                    this.banner = result.indexAd;
+                })
+        }
+    }
 }
 </script>
 <style lang='less' scoped>
-
+.city-search{
+    
+    height: 44px;
+    
+    padding: 0 .2rem;
+    .current-city{
+        width: 2.5rem;
+        font-size: 14px;
+    }
+    .search-box{
+        flex: 1;
+        padding: .1rem;
+        color: rgba(255, 255, 255, .2);
+        background-color: currentColor;
+        border-radius: 5px;
+        &:last-child{
+            font-size: 14px;
+        }
+    }
+}
+.type-list{
+    padding-bottom: .2rem;
+    margin-bottom: .1rem;
+    background-color: #fff;
+    .type-item{
+        width: 25%;
+        a{
+            display: block;
+        }
+        img{
+            width: 100%;
+            padding: .2rem .4rem .15rem .4rem;
+        }
+        p{
+            color: #707070;
+        }
+    }
+}
 </style>
