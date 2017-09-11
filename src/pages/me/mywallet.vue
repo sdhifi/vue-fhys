4cbd15eb5a57bf6b1d1eca65acf9baa5539bf483
hishion <956656628@qq.com>
2017-09-11 18:01:01 +0800

    我的钱包

--- src/pages/me/mywallet.vue
+++ src/pages/me/mywallet.vue
@@ -7,10 +7,29 @@
           <div class="tab-item tab-active">个人福利</div>
           <div class="tab-item">商户福利</div>
         </nav>
-        <div>
+        <div class="wallet-today">
           今日福利
-        <p>+{{info.benefitYesterdayMoney}}</p>
+          <p>+{{info.benefitYesterdayMoney}}</p>
         </div>
+
+      </section>
+      <section class="wallet-tab">
+        <ul class="flex text-center">
+          <li v-for="(item,index) in tabs" :key="index" class="tab-item">
+            <a href="" v-if="index<3" class="danger-bg">
+              <p>{{item.text}}</p>
+              <p>{{info[item.param]}}</p>
+            </a>
+            <a href="" v-else>
+              <p>{{item.text}}</p>
+              <p class="danger-color">{{info[item.param]}}</p>
+            </a>
+          </li>
+        </ul>
+      </section>
+      <section class="operate-container">
+        <p>*转移说明：可转余额金额 ￥0.00（转余额扣除10%税费）。</p>
+        <yd-button type="warning" size="large">一键转余额</yd-button>
       </section>
       <yd-grids-group :rows="3">
         <yd-grids-item v-for="(item,index) in menu" :key="index" :link="item.link">
@@ -24,49 +43,82 @@
 <script>
 import HeaderTop from 'components/header/index'
 import { countMemberInfo } from '../../api/index'
-import { mixin,getStore } from 'components/common/mixin'
+import { mixin, getStore } from 'components/common/mixin'
 export default {
   name: 'MyWallet',
   data() {
     return {
       info: {},
-      type:'0',
+      type: '0',
+      tabs: [{
+        text: '可参与福利权益',
+        param: 'canParticipate',
+        link: '',
+      }, {
+        text: '责任消费',
+        param: 'canMoney',
+        link: '',
+      }, {
+        text: '还可获得福利',
+        param: 'consumptionMoney',
+        link: '',
+      }, {
+        text: '销售福利',
+        param: 'dividendsTotalMoney',
+        link: '',
+      }, {
+        text: '累计消费',
+        param: 'cumulativeMoney',
+        link: '',
+      }, {
+        text: '余额账户',
+        param: 'balanceMoney',
+        link: '',
+      }, {
+        text: '福利账户余额',
+        param: 'balanceMoney',
+        link: '',
+      }, {
+        text: '销售福利余额',
+        param: 'balanceMoney',
+        link: '',
+      }],
       menu: [
         {
           icon: 'self-tiqufuli',
           text: '提取福利',
           link: '/trade/withdrawals',
-          color:'#33ee44'
+          color: '#33ee44'
         },
         {
           icon: 'self-tiqulishi',
           text: '提取历史',
           link: '/trade/cashhistory',
-          color:'#663355'
+          color: '#663355'
         },
         {
           icon: 'self-yinhangka',
           text: '银行卡',
           link: '/trade/bancard',
-          color:'#ee3355'
+          color: '#ee3355'
         },
         {
           icon: 'self-fulijilu',
           text: '福利记录',
           link: '/trade/welfrecord',
-          color:'#ee3355'
+          color: '#ee3355'
         },
         {
           icon: 'self-xiaofeijilu',
           text: '消费记录',
           link: '/trade/consumerecord',
-          color:'#663355'
+          color: '#663355'
         },
         {
           icon: 'self-xiaofeiguize',
           text: '消费规则',
           link: '/trade/consumerule',
-          color:'#a58a19'
+          color: '#a58a19'
         },
       ]
     }
@@ -83,19 +135,19 @@ export default {
     this.getInfo();
   },
   methods: {
-    getInfo(){
+    getInfo() {
       let vm = this;
       mui.ajax({
         url: countMemberInfo,
         type: 'post',
-        headers: {'app-version': 'v1.0'},
+        headers: { 'app-version': 'v1.0' },
         data: {
-          account:getStore('account'),
-          type:this.type,
-          token:md5(`countMemberInfo${getStore('account')}${this.type}`)
+          account: getStore('account'),
+          type: this.type,
+          token: md5(`countMemberInfo${getStore('account')}${this.type}`)
         },
-        success(res){
-          vm.info= res.result
+        success(res) {
+          vm.info = res.result
         }
       })
     }
@@ -109,9 +161,30 @@ export default {
   background-size: cover;
   color: @white;
   font-size: 14px;
-p{
-  color:#f6ff00;
-  font-size: 20px;
+  p {
+    color: #f6ff00;
+    font-size: 20px;
+  }
 }
+
+.wallet-tab {
+  background-color: @white;
+  border-top: 1px solid #dfdfdf;
+  .tab-item {
+    width: 33.3%;
+    a {
+      .pd-v;
+      display: block;
+    }
+    +.tab-item{
+      border-left: 1px solid #fff;
+    }
+  }
+}
+
+.operate-container {
+  .pd;
+  .mg-v;
+  background-color: @white;
 }
 </style>
