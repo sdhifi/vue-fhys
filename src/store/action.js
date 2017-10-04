816158b7717e8f498ff4365bc0a9054d3c8f0f5f
hishion <956656628@qq.com>
2017-10-04 16:23:27 +0800

    地址管理-action

--- src/store/action.js
+++ src/store/action.js
@@ -6,6 +6,7 @@ import {
 } from './mutation-types'
 import {
   myBanks,
+  getMyAddress,
 } from '../api/index'
 export default {
   getBankList({
@@ -35,5 +36,31 @@ export default {
       }
     })
   },
-  
+  getAddressList({
+    commit,
+    state
+  }) {
+    mui.ajax({
+      url: getMyAddress,
+      type: 'post',
+      headers: {
+        'app-version': 'v1.0'
+      },
+      data: {
+        account: state.account,
+        token: md5('getMyAddress')
+      },
+      success(res) {
+        let _result = res.result;
+        if (_result.length) {
+          _result.forEach((item, index) => {
+            if (item.isDefault == '1') {
+              commit('RECORD_DEFAULT_ADDRESS', item)
+            }
+          })
+          commit('RECORD_ADDRESS_List', _result)
+        }
+      }
+    })
+  },
 }
