e3e187e9c03e845d80d5d8ba10df09b0a5ec5ff6
hishion <956656628@qq.com>
2017-12-19 15:14:20 +0800

    凤凰宝

---
+++ src/pages/treasure/list.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <header-top title="交易记录"></header-top>
+    <main class='scroll-content-2'>
+
+    </main>
+  </div>
+</template>
+<script>
+import HeaderTop from 'components/header/index'
+import { } from '../../api/index'
+export default {
+  name: 'TreasureList',
+  data() {
+    return {
+
+    }
+  },
+  components: { HeaderTop },
+  computed: {
+
+  },
+  created() {
+
+  },
+  activated() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+<style lang='less' scoped>
+@import '../../style/mixin.less';
+
+
+</style>
