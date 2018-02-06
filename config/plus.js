e39e2b1fa7ef33db18f99ea8f4f14dd4cf7a7266
hishion <956656628@qq.com>
2018-02-06 16:32:19 +0800

    原生方法测试文件

---
+++ config/plus.js
@@ -0,0 +1,40 @@
+let plus = {
+  runtime: {
+    openURL(href) {
+      window.location = href;
+    },
+    appid: 'abcde',
+    getProperty(appid, callback) {
+      let info = {
+        appid: "abcde",
+        version: "2.0",
+        name: "hello",
+        description: "this is a test",
+        author: "hishion",
+        email: "hishion.wei@gmail.com",
+        license: "hello world",
+        licensehref: "https://www.github.com/",
+        features: "yes"
+      }
+      callback(info);
+    }
+  },
+  payment: {
+    channels: [{
+      appStoreReceipt: () => {},
+      description: "支付宝",
+      id: "alipay",
+      installService: () => {},
+      requestOrder(ids, successCB1, errorCB1) {},
+      restoreComplateRequest(options, successCB2) {},
+      serviceReady: true
+    }],
+    getChannels(successCB, errorCB) {
+      successCB(this.channels);
+    },
+    request(channel,statement,successCB,errorCB) {
+      successCB("ok");
+    }
+  }
+};
+exports.plus = plus;
