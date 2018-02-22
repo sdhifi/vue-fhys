ef57cb07a039aa4e2d4b97ce0b085acdecdf9d72
hishion <956656628@qq.com>
2018-02-22 16:45:22 +0800

    天猫打开优惠券方式

--- src/pages/online/tmindex.vue
+++ src/pages/online/tmindex.vue
@@ -190,8 +190,9 @@ export default {
       let url = `http://aihua.likecs.com/index.php?mod=aihua&act=fenghuang&param=detail&id=${pdId}&userid=${userId}&phone=${this
         .member
         .mobile}&email=102286545@qq.com&kh=fenghuang&tbnum=4654646465465`;
-      //this.$router.push({ name: "TMDetail", params: { url } });
-      plus.runtime.openURL(url);
+      mui.os.ios
+        ? plus.runtime.openURL(url)
+        : this.$router.push({ name: "TMDetail", params: { url } });
     }
   }
 };
